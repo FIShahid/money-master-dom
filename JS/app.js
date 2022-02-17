a0b957678a66f6052f6806364739b71e8218e1a0
Fokhrul Islam <shahidcse12@gmail.com>
2022-02-17 22:22:19 +0600

    final push

--- JS/app.js
+++ JS/app.js
@@ -23,24 +23,28 @@ document.getElementById('calculate-button').addEventListener('click', function (
 
     balance.innerText = incomeInput;
 
-    let  updatedBalance = parseFloat(incomeInput) - parseFloat(finalExpences);
+
+
+    let updatedBalance = parseFloat(incomeInput) - parseFloat(finalExpences);
     document.getElementById('total-balance').innerText = updatedBalance;
 
     //Error Handling
-    if (foodInput < 0|| rentInput < 0 || clothInput < 0) {
+    if (foodInput < 0 || rentInput < 0 || clothInput < 0) {
         alert('Input Field Can not Be Negative');
         let totalExpenses = document.getElementById('total-expences');
         totalExpenses.innerText = '00';
-       let getBalance = document.getElementById('total-balance');
-       getBalance.innerText = '00';
+        let getBalance = document.getElementById('total-balance');
+        getBalance.innerText = '00';
 
     }
+
+
     if (finalExpences > incomeInput) {
         alert('You Do Not Have Sufficient Amount');
         let totalExpenses = document.getElementById('total-expences');
         totalExpenses.innerText = '00';
-       let getBalance = document.getElementById('total-balance');
-       getBalance.innerText = '00';
+        let getBalance = document.getElementById('total-balance');
+        getBalance.innerText = '00';
 
     }
 });
@@ -60,23 +64,23 @@ document.getElementById('save-button').addEventListener('click', function () {
 
     let remainingBalance = document.getElementById('remaining-balance');
     let newBalance = document.getElementById('total-balance').innerText;
-  
+
     remainingBalance.innerText = parseFloat(newBalance) - parseFloat(savingCalculation);
-      //Error Handling
-      if (savingCalculation > newBalance) {
+    //Error Handling
+    if (savingCalculation > newBalance) {
         alert('You do not have Enough Balance');
-       let savingBalance = document.getElementById('saving-balance');
-       savingBalance.innerText = '00';
-       let remainingBalance = document.getElementById('remaining-balance');
-       remainingBalance.innerText = '00';
+        let savingBalance = document.getElementById('saving-balance');
+        savingBalance.innerText = '00';
+        let remainingBalance = document.getElementById('remaining-balance');
+        remainingBalance.innerText = '00';
 
     }
-      if (savingCalculation < 0) {
+    if (savingCalculation < 0) {
         alert('You can not Input negative value');
-       let savingBalance = document.getElementById('saving-balance');
-       savingBalance.innerText = '00';
-       let remainingBalance = document.getElementById('remaining-balance');
-       remainingBalance.innerText = '00';
+        let savingBalance = document.getElementById('saving-balance');
+        savingBalance.innerText = '00';
+        let remainingBalance = document.getElementById('remaining-balance');
+        remainingBalance.innerText = '00';
 
     }
 
