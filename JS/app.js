a619445a3caa403f08234bd98a438c6b16abaacd
Fokhrul Islam <shahidcse12@gmail.com>
2022-02-17 15:11:55 +0600

    total calculation

--- JS/app.js
+++ JS/app.js
@@ -1,9 +1,4 @@
-// function inputField(item){
-//     const foodInput = document.getElementById( item +'-input').value;
-// }
-
-
-document.getElementById('calculate-button').addEventListener('click',function(){
+document.getElementById('calculate-button').addEventListener('click', function () {
 
     //get expense value
     const foodInput = document.getElementById('food-input').value;
@@ -11,45 +6,31 @@ document.getElementById('calculate-button').addEventListener('click',function(){
     const rentInput = document.getElementById('rent-input').value;
     const clothInput = document.getElementById('cloth-input').value;
     const totalExpenses = document.getElementById('total-expences');
-   const finalExpences= totalExpenses.innerText = parseFloat (foodInput) + parseFloat (rentInput) + parseFloat (clothInput);
-      
+    const finalExpences = totalExpenses.innerText = parseFloat(foodInput) + parseFloat(rentInput) + parseFloat(clothInput);
+
     //get Income Value
 
     const incomeInput = document.getElementById('income-input').value;
     const balance = document.getElementById('total-balance');
-    
+
     balance.innerText = incomeInput;
 
-   let updatedBalance = parseFloat (incomeInput) - parseFloat (finalExpences);
-   document.getElementById('total-balance').innerText = updatedBalance;
-
-  
-    
+    let updatedBalance = parseFloat(incomeInput) - parseFloat(finalExpences);
+    document.getElementById('total-balance').innerText = updatedBalance;
 })
 
-// function calculateSaving(percent){
-
-//     let result = parseInt(incomeInput) * parseInt (percent/100);
-//     return result;
-
-// }
-
 //Saving Calculation
-document.getElementById('save-button').addEventListener('click',function(){
+document.getElementById('save-button').addEventListener('click', function () {
     const incomeInput = document.getElementById('income-input').value;
     let savingPercentage = document.getElementById('saving-percent').value;
-    let percent = incomeInput * (savingPercentage/100);
-    
+    let percent = incomeInput * (savingPercentage / 100);
+
     document.getElementById('saving-balance').innerText = percent;
 
-//Remaining Balance
+    //Remaining Balance
 
     let remainingBalance = document.getElementById('remaining-balance');
     let newBalance = document.getElementById('total-balance').innerText;
-    // console.log(newBalance);
-    remainingBalance.innerText = parseFloat(newBalance) - parseFloat(percent);        
+    remainingBalance.innerText = parseFloat(newBalance) - parseFloat(percent);
 
- 
-
-    
 })
